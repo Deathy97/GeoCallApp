fb284d0c8d213c7ae772567c35aeab348fc27326
Deathy97 <rafatorrea@hotmail.com>
2018-03-18 14:26:39 +0100

    First try to add a geofence, not working yet

--- MyApplication/app/src/main/java/com/example/rafa/geo/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/rafa/geo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rafa.geo;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -10,31 +11,31 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+
 public class MainActivity extends Activity {
 
-    TextView tv1;
-    TextView tv2;
-    TextView tv3;
-    TextView tv4;
+    TextView tv1, tv2, tv3, tv4;
     Geocoder geocoder;
     List<Address> addresses;
-    Location location;
     LocationManager locationManager;
-    LocationListener locationListener;
-    double latitudActual, longitudActual;
-    String numero = "";
+    List<Geofence> mGeofenceList =  new ArrayList<>();
+    PendingIntent mGeofencePendingIntent;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
 
@@ -49,7 +50,10 @@ public class MainActivity extends Activity {
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         LocationListener locationListener = new MyLocationListener();
+
         geocoder = new Geocoder(this, Locale.getDefault());
+
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.CALL_PHONE}, 1234);
 
         //Controlando las versiones, a partir de Android Mn el request de los permisos se hace al ejecutar no al instalar
         //Obteniendo la ultima posicion conocida para partir sobre ella como base
@@ -66,11 +70,47 @@ public class MainActivity extends Activity {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
         }
 
-        if ((Math.round(latitudActual * 10000d) / 10000d) == (Math.round(latitud * 10000d) / 10000d) && (Math.round(longitudActual * 10000d) / 10000d) == (Math.round(longitud * 10000d) / 10000d)){
-            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+numero));
-        }
 
+        mGeofenceList.add(new Geofence.Builder()
+                .setRequestId("Geofence1")
+                .setCircularRegion(41.632730 ,-0.890711,100)
+                .setExpirationDuration(5000)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build());
+
+
+        GeofencingClient mGeofencingClient = null;
+        mGeofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                    }
+                });
     }
+
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(mGeofenceList);
+        return builder.build();
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        mGeofencePendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return mGeofencePendingIntent;
+    }
+
+
 
     public class MyLocationListener implements LocationListener {
 
@@ -104,7 +144,5 @@ public class MainActivity extends Activity {
                 Toast.makeText(getApplicationContext(), "GPS disable", Toast.LENGTH_LONG).show();
             }
         };
-
-
 }
 
